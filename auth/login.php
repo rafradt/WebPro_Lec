ef55c3dec350a9eba0f47610f7048ab2131f54cd
Cryoonic <albertalexander2004@gmail.com>
2024-10-22 11:43:49 +0700

    login modified

--- auth/login.php
+++ auth/login.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../index.css">
     <style>
         body{
             background-color: whitesmoke;
